af280e5a7c454f2ce5ae493679004824aebb12b7
GeovaneMartinszz <geovanemartins2305@gmail.com>
2024-11-18 10:16:19 -0300

    .

---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import Flask, render_template
+
+app = Flask(__name__,
+            template_folder="siteciencia/templates", static_folder="siteciencia/static")
+
+@app.route("/")
+def index():
+    # return "<h1>Bom Dia, professor.</h1>"
+    return render_template("Index.html")
